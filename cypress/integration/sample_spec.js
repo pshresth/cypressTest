1d1b90ca33fda74a938c9dfb51b1e42103d4137c
pshresth <pshrestha@peoplenetonline.com>
2018-03-16 17:12:32 -0500

    add: param file, refactor: sample_spec refactored sample_test.js to mimic login.js test from OEM-Automation

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,17 +2,27 @@
 describe("My First test", function (){
 
     var dash = require('../utilities/commonUtility.js');
+    var emails = require('../utilities/params.js');
+    var loginUserArray = emails.userLoginArray;
 
-    it('Logs into the site', function () {
-        cy.visit('http://paccar-portal-qa.connectedfleet.io');
-        cy.get('[name="email"]')
-          .type('paccaradmin@test.com');
-        cy.get('[name="password"]')
-          .type('Password1');
-        cy.get('[type=submit]').click();
-        cy.url()
-          .should('include', '#/nav/dashboard');
-        cy.get(dash.dogLink).click();
-        dash.clickCustomerLink();
-    });
+    for (var i = 1; i <= 50; i++) {
+        it('Logs into the site', function () {
+            cy.visit('http://paccar-portal-qa.connectedfleet.io');
+            cy.get('[name="email"]')
+              .type(loginUserArray[Math.floor(Math.random() * loginUserArray.length)]);
+            cy.get('[name="password"]')
+              .type('Password1');
+            cy.get('[type=submit]')
+                .click();
+            cy.url()
+              .should('include', '#/nav/dashboard');
+            cy.get('[id="user-profile-menu-name"]', {timeout: 6000})
+              .should('be.visible');
+            cy.get('[id="user-profile-menu-name"]').click();
+            cy.get('[href="#/logout"]', {timeout: 6000})
+              .should('be.visible');
+            cy.get('[href="#/logout"]').click();
+            cy.clearLocalStorage();
+        });
+    }
 });
---
+++ cypress/utilities/params.js
@@ -0,0 +1,41 @@
+var params = function () {
+
+    this.peoplenetadmin = 'peoplenetadmin-automation@test.com';
+    this.paccaradmin = 'paccaradmin-automation@test.com';
+    this.kenmexpaccaradmin = 'kenmexpaccaradmin-automation@test.com';
+    this.paccaruser = 'paccaruser-automation@test.com';
+    this.paccarpoweruser = 'paccarpoweruser-automation@test.com';
+    this.divisionuser = 'peterbiltdivision-automation@test.com';
+    this.divisionuser1 = 'kenworthdivision-automation@test.com';
+    this.cumminsuser = 'cumminsuser-automation@test.com';
+    this.dealerowneradmin = 'dealerowneradmin-automation@test.com';
+    this.dealerowneruser = 'dealerowneruser-automation@test.com';
+    this.dealerregionadmin = 'dealerregionadmin-automation@test.com';
+    this.dealerregionuser = 'dealerregionuser-automation@test.com';
+    this.dealerregionadmin2 = 'dealerregionadmin2-automation@test.com';
+    this.dealerregionuser2 = 'dealerregionuser2-automation@test.com';
+    this.factoryworker = 'factoryworker-automation@test.com';
+    this.dealeradmin = 'dealeradmin-automation@test.com';
+    this.dealeruser = 'dealeruser-automation@test.com';
+    this.changeuser = 'changePasswordAutamationUser@test.com';
+    this.customeradmin = 'customeradmin-automation@test.com';
+    this.customeradmincummins = 'customeradmincummins-automation@test.com';
+    this.preferredcustomeradmin ='preferredcustomeradin-automation@test.com';
+    this.preferredcustomeruser ='preferredcustomeruser-automation@test.com';
+    this.customeradmin2 = 'customeradmin@test.com';
+    this.customeruser = 'customeruser-automation@test.com';
+    this.customerusercummins = 'customerusercummins-automation@test.com';
+    this.customeruser2 = 'customeruser@test.com';
+    this.dealertech = 'dealertech-automation@test.com';
+    this.tsr1 = 'tsr1-automation@test.com';
+    this.tsr2 = 'tsr2-automation@test.com';
+    this.lockOutUser = 'lockoutuser-automation@test.com';
+
+    this.userLoginArray = [this.peoplenetadmin, this.paccaradmin, this.paccarpoweruser,
+        this.paccaruser, this.divisionuser, this.cumminsuser, this.dealerowneradmin, this.dealerowneruser,
+        this.dealerregionadmin, this.dealerregionuser, this.factoryworker, this.dealeradmin,
+        this.dealeruser, this.dealertech, this.customeradmin, this.customeruser];
+
+};
+
+module.exports = new params();
